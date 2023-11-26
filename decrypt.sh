25255a9aca655b1ba157a08b26072ed0aeb6b43b
zadjadr <zadjad.rezai@ioki.com>
2023-11-26 04:36:10 +0100

    Encryption: Ignore wireguard/config

--- decrypt.sh
+++ decrypt.sh
@@ -5,12 +5,13 @@
 search_directory="."
 
 # Find all files with .env extension in the specified directory and its subdirectories
-env_files=$(find "$search_directory" -type f -name "*.env")
+env_files=$(find "$search_directory" -type f -name "*.env" -not -path "$search_directory/tools/wireguard/config/*" 2>/dev/null)
 
 # Loop through each .env file and run the sops command
 for file_name in $env_files; do
     decrypted_file="$file_name.dec"
 
+    rm -f $decrypted_file
     echo "Decrypting $file_name into $decrypted_file..."
     # Run the sops command and save the decrypted content to the new file
     sops -d "$file_name" > "$decrypted_file"
--- encrypt.sh
+++ encrypt.sh
@@ -5,7 +5,7 @@
 search_directory="."
 
 # Find all files with .env extension in the specified directory and its subdirectories
-env_files=$(find "$search_directory" -type f -name "*.env")
+env_files=$(find "$search_directory" -type f -name "*.env" -not -path "$search_directory/tools/wireguard/config/*" 2>/dev/null)
 
 # Loop through each .env file and run the sops command
 for file_name in $env_files; do
